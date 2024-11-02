30eef82ea40ff215e0e93186807edc7e6bbbdc7b
me-danix <tsilik9801@yandex.ru>
2024-11-02 18:41:57 +0500

    Homework is done

---
+++ src/Bicycle.java
@@ -0,0 +1,6 @@
+public class Bicycle extends WheelTransport {
+
+    public Bicycle (String modelName) {
+        super(modelName, 2);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car("car1");
+        Car car2 = new Car("car2");
+
+        Truck truck = new Truck("truck1", 6);
+        Truck truck2 = new Truck("truck2", 8);
+
+        Bicycle bicycle = new Bicycle("bicycle1");
+        Bicycle bicycle2 = new Bicycle("bicycle2");
+
+        Transport[] transports = {car, car2, truck, truck2, bicycle, bicycle2};
+
+        ServiceStation station = new ServiceStation();
+
+        for (Transport transport : transports) {
+            station.check(transport);
+            System.out.println();
+        }
     }
 }
